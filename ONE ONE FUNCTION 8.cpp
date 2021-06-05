9bd50bfdb78974d593b4a8783e1271a7e0aa22ba
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2021-06-05 21:22:38 +0530

    Add files via upload

---
+++ ONE ONE FUNCTION 8.cpp
@@ -0,0 +1,56 @@
+----------------------------- ONE ONE FUNCTION--------------------------------------------------------------------------------------------------------------------------------
+
+
+
+OUTPUT -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include <cmath>
+
+#include <cstdio>
+
+#include <vector>
+
+#include <iostream>
+
+#include <algorithm>
+
+using namespace std;
+
+int fact(int n)
+
+{
+    
+if(n<=1)
+    
+{
+       
+ return 1;
+    
+}
+    
+else
+       
+ return n*fact(n-1);
+
+}
+
+
+int main() 
+{
+    
+int n,m,temp;
+    
+cin>>m>>n;
+    
+int f=fact(m);
+    
+int a=fact(m-n);
+   
+ temp=f/a;
+   
+ cout<<temp;
+    
+    
+ return 0;
+
+}
