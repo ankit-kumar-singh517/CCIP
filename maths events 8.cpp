6c31c9c8766b476c4c0a0bb9a0847aa1b76f6e7a
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2021-06-05 21:22:24 +0530

    Add files via upload

---
+++ maths events 8.cpp
@@ -0,0 +1,56 @@
+=====================================================================MATHS EVENTS 1===============================
+
+Given two martix of size (M X N) & (N X P). Find out the number of addition operation require to multiply two matrices.
+
+Input Format
+
+Enter the value of M,N & P.
+
+Constraints
+
+1 <= M,N,P <= 10
+
+Output Format
+
+Print the desired output
+
+Sample Input 0
+
+3 2 3
+Sample Output 0
+
+18
+Sample Input 1
+
+2 2 3
+Sample Output 1
+
+12
+
+
+OUTPUT==================================================================================================================    
+
+#include <cmath>
+#include <cstdio>
+
+#include <vector>
+
+#include <iostream>
+
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int a,b,c;
+
+    cin>>a>>b>>c;
+
+    cout<<a*b*c;
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+
+    return 0;
+
+}
